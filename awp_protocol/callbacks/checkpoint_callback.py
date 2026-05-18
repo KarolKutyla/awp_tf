3a31c72250c1a45ba85d8cec15817553f61a923d
Karol Kutyła <103074417+JonaszPawulon@users.noreply.github.com>
2026-05-18 16:58:26 +0200

    initinit

--- awp_protocol/callbacks/checkpoint_callback.py
+++ awp_protocol/callbacks/checkpoint_callback.py
@@ -4,16 +4,18 @@ import tensorflow as tf
 
 class EpochCheckpoint(tf.keras.callbacks.Callback):
 
-    def __init__(self, save_dir, verbose=False):
+    def __init__(self, save_dir, interval = 10, verbose=False):
         super().__init__()
-        self.save_dir = save_dir
+        self._save_dir = save_dir
         self._verbose = verbose
-        os.makedirs(save_dir, exist_ok=True)
+        self._interval = interval
+        os.makedirs(self._save_dir, exist_ok=True)
 
 
     def on_epoch_end(self, epoch, logs=None):
-        path = os.path.join(self.save_dir, f"epoch_{epoch}")
-        self.model.save(path, include_optimizer=True)
+        if epoch % self._interval == 0:
+            path = os.path.join(self._save_dir, f"epoch_{epoch}.keras")
+            self.model.save(path, include_optimizer=True)
 
-        if self._verbose:
-            print(f"\n[Checkpoint] saved to {path}")
+            if self._verbose:
+                print(f"\n[Checkpoint] saved to {path}")
