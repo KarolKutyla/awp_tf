df26b44794cda80e9207eaa38f66ed11a797b168
Karol Kutyła <103074417+JonaszPawulon@users.noreply.github.com>
2026-08-18 01:51:41 +0200

    initinit

---
+++ awp_tf/attacks/norm_and_std/pgd.py
@@ -0,0 +1,139 @@
+import tensorflow as tf
+
+from dataclasses import dataclass, replace
+
+from awp_tf.attacks.attack import TensorflowEvasionAttack
+
+
+@dataclass(frozen=True)
+class PGDParams:
+    perturbation_bound: float = 8 / 255
+    pgd_step: int = 10
+    pgd_step_size: float = 2 / 255
+    norm: str = "linf"
+
+
+class PGDAttack(TensorflowEvasionAttack):
+    def __init__(
+            self,
+            model: tf.keras.Model,
+            params: PGDParams | None = None,
+            **overrides
+    ):
+        super().__init__(model)
+        self._dtype = tf.float32
+        self._params = params or PGDParams()
+        self._params = replace(self._params, **overrides)
+        self._perturbation_bound: tf.Tensor
+        self._pgd_step_size: tf.Tensor
+
+        self._pgd_step = tf.constant(self._params.pgd_step, dtype=tf.int32)
+        self._perturbation_bound = tf.constant(self._params.perturbation_bound * 2.0, dtype=self._dtype)
+        self._pgd_step_size = tf.constant(self._params.pgd_step_size * 2.0, dtype=self._dtype)
+        self._norm = self._params.norm
+
+
+    # @tf.function(reduce_retracing=True)
+    def generate(self, x_batch: tf.Tensor, y_batch: tf.Tensor) -> tf.Tensor:
+        if self._norm == "linf":
+            return self._generate_inf(x_batch, y_batch)
+        if self._norm == "l2":
+            return self._generate_l2(x_batch, y_batch)
+        if self._norm == "l1":
+            raise Exception("Norm l1 not implemented")
+        raise Exception(f"Unknown norm type: {self._params.norm}. Should be one of: linf, l2, l1.")
+
+
+    def _generate_l2(self, x_batch: tf.Tensor, y_batch: tf.Tensor) -> tf.Tensor:
+        x_adv = self._random_sample(x_batch)
+        i0 = tf.constant(0, dtype=tf.int32)
+        norm_indices = tuple(range(1, len(x_batch.shape)))
+        def cond(i, x):
+            return i < self._pgd_step
+
+        def body(i, x):
+            x = self._pgd_l2_step(x_batch, x, y_batch, norm_indices)
+            return i + 1, x
+
+        _, x_adv = tf.nest.map_structure(
+            tf.stop_gradient,
+            tf.while_loop(cond, body, [i0, x_adv], parallel_iterations=1))
+        return x_adv
+
+
+    def _pgd_l2_step(self, x: tf.Tensor, x_adv: tf.Tensor, y: tf.Tensor, norm_indices: tuple) -> tf.Tensor:
+        gradient = self._calculate_gradient(x_adv, y)
+        gradient_norm = tf.sqrt(tf.reduce_sum(tf.square(gradient), axis=norm_indices, keepdims=True))
+        gradient = (tf.math.divide_no_nan(gradient, gradient_norm))
+        x_adv = x_adv + gradient * self._pgd_step_size
+
+        perturbation = x_adv - x
+        perturbation = self._project_l2(perturbation, norm_indices)
+        x_adv = x + perturbation
+        return x_adv
+
+
+    def _project_l2(self, perturbation, norm_indices: tuple):
+        pert_norm = tf.sqrt(tf.reduce_sum(tf.square(perturbation), axis=norm_indices, keepdims=True))
+        factor_ones = tf.ones_like(pert_norm)
+        factor_bounds = tf.ones_like(pert_norm) * self._perturbation_bound
+
+        factor = tf.minimum(
+            factor_ones,
+            tf.math.divide_no_nan(factor_bounds, pert_norm)
+        )
+        return perturbation * factor
+
+
+    def _generate_inf(self, x_batch: tf.Tensor, y_batch: tf.Tensor) -> tf.Tensor:
+        x_adv = self._random_sample(x_batch)
+        i0 = tf.constant(0, dtype=tf.int32)
+        def cond(i, x):
+            return i < self._pgd_step
+
+        def body(i, x):
+            x = self._pgd_linf_step(x_batch, x, y_batch)
+            return i + 1, x
+
+        _, x_adv = tf.nest.map_structure(
+            tf.stop_gradient,
+            tf.while_loop(cond, body, [i0, x_adv], parallel_iterations=1))
+        return x_adv
+
+
+    def _pgd_linf_step(self, x: tf.Tensor, x_adv: tf.Tensor, y: tf.Tensor) -> tf.Tensor:
+        gradient = self._calculate_gradient(x_adv, y)
+        gradient = tf.sign(gradient)
+        x_adv = x_adv + gradient * self._pgd_step_size
+
+        perturbation = x_adv - x
+        perturbation = self._project_linf(perturbation)
+        x_adv = x + perturbation
+        return x_adv
+
+
+    def _project_linf(self, perturbation) -> tf.Tensor:
+        return tf.clip_by_value(
+            perturbation,
+            -self._perturbation_bound,
+            self._perturbation_bound
+        )
+
+
+    def _calculate_gradient(self, x_adv: tf.Tensor, y: tf.Tensor) -> tf.Tensor:
+        with tf.GradientTape() as tape:
+            tape.watch(x_adv)
+            logits = self.model(x_adv, training=False)
+            loss = tf.keras.losses.sparse_categorical_crossentropy(y, logits, from_logits=True)
+            loss = tf.reduce_mean(loss)
+        gradient = tape.gradient(loss, x_adv)
+        return gradient
+
+
+    def _random_sample(self, x_batch) -> tf.Tensor:
+        x_adv = x_batch + tf.random.uniform(shape=tf.shape(x_batch), minval=-self._perturbation_bound, maxval=self._perturbation_bound, dtype=self._dtype)
+        return x_adv
+
+
+    def project_l1(self):
+        ...
