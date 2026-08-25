8ef8eb58341f1d89b74973af27340cf2e1a0250a
Karol Kutyła <103074417+JonaszPawulon@users.noreply.github.com>
2026-08-25 13:32:10 +0200

    initinit

---
+++ awp_tf/double_update_awp/awp.py
@@ -0,0 +1,258 @@
+from __future__ import absolute_import, division, print_function, unicode_literals, annotations
+
+import time
+from dataclasses import dataclass, replace
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import Callback
+
+from awp_tf.double_update_awp.batch_processor import BatchProcessor, AWPParams
+from awp_tf.attacks.attack import TensorflowEvasionAttack
+from awp_tf.callbacks.progbar_logger import ProgbarLogger
+from awp_tf.callbacks.checkpoint_callback import EpochCheckpoint
+from awp_tf.losses.loss import AdversarialLoss
+from awp_tf.losses.trades_loss import TradesLoss
+from awp_tf.losses.adversarial_categorical_cross_entropy import AdversarialSparseCategoricalCrossEntropy
+
+
+@dataclass(frozen=True)
+class Params:
+    mode: str = "trades"
+    regularization_parameter: int = 6
+    protocol_params: AWPParams = AWPParams()
+
+class Trainer:
+
+    def __init__(
+            self,
+            classifier: tf.keras.Model,
+            attack: TensorflowEvasionAttack,
+            warmup: int = 0,
+            adversarial_loss: AdversarialLoss | None = None,
+            trained_layers: tuple[bool | float, ...] | None = None,
+            params: Params | None = None,
+            subset_enabled = False,
+            **overrides
+    ):
+        self._fast_mode = True
+
+        self._params = params or Params()
+        self._params = replace(self._params, **overrides)
+
+        self._classifier: tf.keras.Model = classifier
+        self._attack: TensorflowEvasionAttack = attack
+        self._warmup: int
+        self._apply_wp: bool
+        self._adversarial_loss: AdversarialLoss | None = adversarial_loss
+        self._tracked_layers: tuple[bool | float, ...] | None = trained_layers
+
+        self._steps_per_epoch: int | None = None
+        self._epochs_run = 0
+        self._trainer: BatchProcessor
+        self._warmup = warmup
+
+        self._progbar: tf.keras.utils.Progbar
+        self._callback_list: tf.keras.callbacks.CallbackList
+        self._logger: ProgbarLogger
+        self._ckpt = EpochCheckpoint(self._classifier.name)
+
+        self._clean_loss_metric = tf.keras.metrics.Mean()
+        self._clean_accuracy_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+        self._robust_loss_metric = tf.keras.metrics.Mean()
+        self._robust_accuracy_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+        self._subset_enabled = subset_enabled
+
+
+    def fit(
+            self,
+            x: tf.Tensor,
+            y: tf.Tensor,
+            validation_data: tuple[tf.Tensor, tf.Tensor] | None = None,
+            batch_size: int = 128,
+            nb_epochs: int = 1,
+            callbacks: list[Callback] | None = None,
+            enable_adversarial = True,
+            **kwargs
+    ):
+        train_dataset = (
+            tf.data.Dataset.from_tensor_slices((x, y))
+            .batch(batch_size, drop_remainder=True)
+            .prefetch(tf.data.AUTOTUNE))
+        self._steps_per_epoch = train_dataset.cardinality().numpy() or None
+
+        validation_dataset = None
+        if validation_data:
+            val_x, val_y = validation_data
+            validation_dataset = (
+                tf.data.Dataset.from_tensor_slices((val_x, val_y))
+                .batch(batch_size, drop_remainder=True)
+                .prefetch(tf.data.AUTOTUNE)
+            )
+
+        self._train_loop(train_dataset, nb_epochs, callbacks=callbacks, validation_dataset=validation_dataset, enable_adversarial=enable_adversarial)
+
+
+    def fit_dataset(
+            self,
+            train_dataset: tf.data.Dataset,
+            validation_dataset: tf.data.Dataset | None = None,
+            nb_epochs: int = 1,
+            callbacks: list[tf.keras.callbacks.Callback] | None = None,
+            enable_adversarial = True,
+            **kwargs
+    ):
+        self._steps_per_epoch = train_dataset.cardinality().numpy() or None
+        self._train_loop(train_dataset, nb_epochs, callbacks=callbacks, validation_dataset=validation_dataset, enable_adversarial=enable_adversarial)
+
+
+    def _train_loop(
+            self,
+            train_dataset,
+            nb_epochs,
+            validation_dataset: tf.data.Dataset | None = None,
+            callbacks: list[tf.keras.callbacks.Callback] | None = None,
+            enable_adversarial=True,
+    ):
+        callbacks = callbacks or []
+        self._logger = ProgbarLogger()
+        callbacks += [self._logger]
+        self._callback_list = tf.keras.callbacks.CallbackList(callbacks, add_history=True, model=self._classifier)
+
+        self._callback_list.on_train_begin()
+        self._trainer = self._init_training_object()
+
+        for epoch in range(nb_epochs):
+            self._epoch(train_dataset, epoch + 1, validation_dataset=validation_dataset, enable_adversarial=enable_adversarial)
+
+        self._callback_list.on_train_end()
+
+
+    def _epoch(self, train_dataset: tf.data.Dataset, epoch: int, validation_dataset: tf.data.Dataset | None = None, enable_adversarial=True):
+        self._reset_metrics()
+
+        self._progbar = tf.keras.utils.Progbar(
+            self._steps_per_epoch,
+            stateful_metrics=["loss", "accuracy", "robust_loss", "robust_accuracy"],
+        )
+        self._logger.update_progbar(self._progbar)
+
+        self._callback_list.on_epoch_begin(self._epochs_run)
+
+        start_time = time.time()
+        warmup = epoch <= self._warmup
+        normal_iter = iter(train_dataset)
+        alt_iter = iter(train_dataset)
+        for step, ((x_batch, y_batch), (x_batch_alt, y_batch_alt)) in enumerate(zip(normal_iter, alt_iter)):
+            self._run_batch(x_batch, y_batch, x_batch_alt, y_batch_alt, step+1, warmup=warmup, enable_adversarial=enable_adversarial)
+        end_time = time.time()
+        train_time = end_time - start_time
+
+        logs = self._collect_train_logs()
+        lr = None
+        if self._classifier.optimizer is not None:
+            lr = self._classifier.optimizer.learning_rate
+        logs.update({
+            "train_time": train_time,
+            "lr": lr,
+        })
+
+        if validation_dataset is not None:
+            self._reset_metrics()
+            start_time = time.time()
+            self._run_validation(validation_dataset)
+            end_time = time.time()
+            validation_time = end_time - start_time
+            logs.update({
+                "val_loss": self._clean_loss_metric.result(),
+                "val_accuracy": self._clean_accuracy_metric.result(),
+                "val_robust_loss": self._robust_loss_metric.result(),
+                "val_robust_accuracy": self._robust_accuracy_metric.result(),
+                "val_time": validation_time,
+            })
+
+        self._progbar.update(self._steps_per_epoch, finalize=True)
+        self._callback_list.on_epoch_end(epoch, logs)
+
+
+    def _run_batch(self, x_batch: tf.Tensor, y_batch: tf.Tensor, x_batch_alt: tf.Tensor, y_batch_alt: tf.Tensor, step, warmup, enable_adversarial=True):
+        self._callback_list.on_batch_begin(step)
+
+        batch_results = self._train_step(x_batch, y_batch, x_batch_alt, y_batch_alt, warmup=warmup, enable_adversarial=enable_adversarial)
+        self._update_metrics(y_batch, batch_results)
+        self._callback_list.on_batch_end(step, self._collect_train_logs())
+
+    def _collect_train_logs(self):
+        logs = {
+            "loss": self._clean_loss_metric.result(),
+            "accuracy": self._clean_accuracy_metric.result(),
+            "robust_loss": self._robust_loss_metric.result(),
+            "robust_accuracy": self._robust_accuracy_metric.result(),
+        }
+        return logs
+
+    def _run_validation(self, validation_dataset):
+        for x_batch, y_batch in validation_dataset:
+            batch_results = self._trainer.validation_step(x_batch, y_batch)
+            self._update_metrics(y_batch, batch_results)
+
+
+    def _update_metrics(self, y_batch, batch_results: tuple):
+        clean_loss, clean_logits, robust_loss, robust_logits = batch_results
+        self._clean_loss_metric.update_state(clean_loss)
+        self._clean_accuracy_metric.update_state(y_batch, clean_logits)
+        self._robust_loss_metric.update_state(robust_loss)
+        self._robust_accuracy_metric.update_state(y_batch, robust_logits)
+
+
+    def _reset_metrics(self):
+        self._clean_loss_metric.reset_state()
+        self._clean_accuracy_metric.reset_state()
+        self._robust_loss_metric.reset_state()
+        self._robust_accuracy_metric.reset_state()
+
+
+    def _train_step(self, x_batch: tf.Tensor, y_batch: tf.Tensor, x_batch_alt: tf.Tensor, y_batch_alt: tf.Tensor, warmup: bool, enable_adversarial=True) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
+        if not enable_adversarial:
+            return self._non_adversarial_step(x_batch, y_batch)
+        if warmup:
+            return self._trainer.adv_train_step(x_batch, y_batch)
+        else:
+            return self._trainer.awp_train_step(x_batch, y_batch, x_batch_alt, y_batch_alt)
+
+
+    @tf.function
+    def _non_adversarial_step(self, x_batch: tf.Tensor, y_batch: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
+        with tf.GradientTape() as tape:
+            logits = self._classifier(x_batch, training=True)
+            loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)(y_batch, logits)
+        gradient = tape.gradient(loss, self._classifier.trainable_variables)
+        self._classifier.optimizer.apply_gradients(zip(gradient, self._classifier.trainable_variables))
+        return loss, logits, loss, logits
+
+
+    def _init_training_object(self):
+        attack = self._attack
+        adversarial_loss = self._adversarial_loss or _select_adversarial_loss(self._params.mode, self._params.regularization_parameter)
+        tracked_layers = self._tracked_layers or select_default_trained_layers_tf(self._classifier)
+
+        return BatchProcessor(
+            self._classifier,
+            attack,
+            adversarial_loss,
+            tracked_layers=tracked_layers,
+            params=self._params.protocol_params
+        )
+
+
+def select_default_trained_layers_tf(classifier: tf.keras.Model) -> tuple[bool, ...]:
+        return tuple('kernel' in variable.name for variable in classifier.trainable_variables)
+
+
+def _select_adversarial_loss(mode: str, alpha = 0.5) -> AdversarialLoss:
+    if mode == "pgd":
+        return AdversarialSparseCategoricalCrossEntropy()
+    if mode == "trades":
+        return TradesLoss(regularization_parameter=alpha)
+    else:
+        raise Exception("Mode not provided! Chose pgd or trades.")
+
---
+++ awp_tf/double_update_awp/weight_calculator.py
@@ -0,0 +1,147 @@
+from dataclasses import dataclass, replace
+
+import keras
+
+from awp_tf.losses.loss import AdversarialLoss
+
+import tensorflow as tf
+
+@dataclass(frozen=True)
+class WeightParams:
+    weight_constraint: float = 1.0e-2
+    alternate_distribution_tradeoff: float = 0.5
+
+
+class WeightCalculator:
+    def __init__(
+            self,
+            classifier: tf.keras.Model,
+            loss: AdversarialLoss,
+            layers_selected_for_weight_perturbation: tuple[bool | float, ...] | None,
+            params: WeightParams | None = None,
+            **overrides
+    ):
+        self.step_size: tf.Tensor
+        self._weight_constraint: tf.Tensor
+        self._data_dtype = classifier.weights[0].dtype
+        self._classifier = classifier
+        self._loss = loss
+
+        self._params = params or WeightParams()
+        self._params = replace(self._params, **overrides)
+        self._weight_constraint = self._params.weight_constraint
+        self._alternate_distribution_tradeoff = tf.clip_by_value(tf.constant(self._params.alternate_distribution_tradeoff, dtype=self._data_dtype), clip_value_min=0.0, clip_value_max=1.0)
+
+        self._perturbation_scales = _normalize_layer_scales(classifier, layers_selected_for_weight_perturbation)
+        self._indices_of_selected_layers: tuple[int, ...] = tuple(i for i, value in enumerate(self._perturbation_scales) if value != 0.0)
+        self._saved_weights: tuple[tf.Variable, ...] = _initiate_memory_for_weight_perturbations(self._classifier, self._indices_of_selected_layers)
+        self._weight_perturbations: tuple[tf.Variable, ...] = _initiate_memory_for_weight_perturbations(self._classifier, self._indices_of_selected_layers)
+        self._weight_norms: tuple[tf.Variable, ...] = _initiate_memory_for_weight_norms(self._weight_perturbations)
+
+
+    def initiate_state_for_batch_process(self) -> None:
+        for i, classifier_idx in enumerate(self._indices_of_selected_layers):
+            self._saved_weights[i].assign(self._classifier.trainable_variables[classifier_idx])
+            self._weight_perturbations[i].assign(tf.zeros_like(self._classifier.trainable_variables[classifier_idx]))
+            self._weight_norms[i].assign(tf.norm(self._classifier.trainable_variables[classifier_idx]))
+
+
+    def apply_weight_perturbations(self):
+        for idx, perturbation, original_weight in zip(self._indices_of_selected_layers, self._weight_perturbations, self._saved_weights):
+            self._classifier.trainable_variables[idx].assign(original_weight.value() + perturbation.value())
+
+
+    def subtract_weight_perturbations(self) -> None:
+        for idx, perturbation in zip(self._indices_of_selected_layers, self._weight_perturbations):
+            self._classifier.trainable_variables[idx].assign_sub(perturbation)
+
+
+    def restore_model(self):
+        for idx, old_value in zip(self._indices_of_selected_layers, self._saved_weights):
+            self._classifier.trainable_variables[idx].assign(old_value)
+
+
+    def calculate_weight_perturbation(self, x: tf.Tensor, y: tf.Tensor, x_adv: tf.Tensor) -> None:
+        gradients = self._calculate_gradient(x, y, x_adv)
+        for idx, gradient, perturbation, norm in zip(self._indices_of_selected_layers, gradients, self._weight_perturbations, self._weight_norms):
+            step_direction = tf.math.divide_no_nan(gradient, tf.norm(gradient))
+            step = step_direction * norm * self._perturbation_scales[idx] * self._weight_constraint
+            perturbation.assign(step)
+
+
+    def calculate_weight_perturbation_with_alt_distribution(self, x: tf.Tensor, y: tf.Tensor, x_adv: tf.Tensor, x_alt: tf.Tensor, y_alt: tf.Tensor, x_adv_alt: tf.Tensor) -> None:
+        gradients = self._calculate_gradient(x, y, x_adv)
+        gradients_alt = self._calculate_gradient(x_alt, y_alt, x_adv_alt)
+        for idx, gradient, gradient_alt, perturbation, norm in zip(self._indices_of_selected_layers, gradients, gradients_alt, self._weight_perturbations, self._weight_norms):
+            alpha = tf.constant(0.5, dtype=self._data_dtype)
+            compared_gradients = tf.math.divide_no_nan(
+                tf.abs(gradient_alt) - tf.abs(gradient),
+                tf.abs(gradient) + tf.abs(gradient_alt)
+            )
+            score = 1.0 + compared_gradients * alpha
+
+            same_sign = (
+                    (gradient == 0)
+                    | (gradient_alt == 0)
+                    | (tf.sign(gradient) == tf.sign(gradient_alt))
+            )
+            mask = tf.cast(same_sign, gradient.dtype)
+
+            step_direction = tf.math.divide_no_nan(gradient, tf.norm(gradient))
+            masked_direction = step_direction * mask
+            step = masked_direction * norm * self._perturbation_scales[idx] * self._weight_constraint
+            scored_step = step * score
+            perturbation.assign(scored_step)
+
+
+
+    def _calculate_gradient(self, x, y, x_adv):
+        selected_variables = tuple(
+            self._classifier.trainable_variables[idx]
+            for idx in self._indices_of_selected_layers
+        )
+        with tf.GradientTape() as tape:
+            loss = self._loss.calculate(x, y, x_adv, self._classifier, training=False)
+        return tape.gradient(loss, selected_variables, unconnected_gradients=tf.UnconnectedGradients.ZERO)
+
+
+
+def _normalize_layer_scales(
+        classifier: tf.keras.Model,
+        scales: tuple[bool | float, ...] | None
+) -> tuple[float, ...]:
+
+    if scales is None:
+        return tuple(
+            1.0 if "kernel" in v.name else 0.0
+            for v in classifier.trainable_variables
+        )
+
+    return tuple(
+        1.0 if x is True else
+        0.0 if x is False else
+        float(x)
+        for x in scales
+    )
+
+def _initiate_memory_for_weight_perturbations(classifier: keras.Model, indices_of_selected_layers: tuple[int, ...]) -> tuple[tf.Variable, ...]:
+    return tuple(
+        tf.Variable(
+            tf.zeros_like(classifier.trainable_weights[idx]),
+            trainable=False,
+        )
+        for idx in indices_of_selected_layers
+    )
+
+
+def _initiate_memory_for_weight_norms(weight_perturbations: tuple[tf.Variable, ...]) -> tuple[tf.Variable, ...]:
+    return tuple(
+        tf.Variable(
+            tf.norm(perturbation), trainable=False
+        )
+        for perturbation in weight_perturbations
+    )
+
+
+def select_default_trained_layers_tf(classifier: tf.keras.Model) -> tuple[bool, ...]:
+    return tuple('kernel' in variable.name for variable in classifier.trainable_variables)
